7f81cdcf1d02caf0224ed5d0b386af45d634bab2
Sammer Alomair <sammeralomair@Sammers-MacBook-Pro.local>
2023-12-22 18:13:24 +0300

    sidebar stuff

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+    return(
+        <div className="">
+            <Head>
+                <title>Twitoor</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto"> 
+                <Sidebar />
+                {/* Feed */}
+                {/* Widgets */}
+                
+                {/* Modal */}
+            </main>
+        </div>
+    );
+}
