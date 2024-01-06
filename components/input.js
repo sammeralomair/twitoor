29611059568f86eb121e5a81b5b4ec0f42017b32
sammeralomair <omair.sao@gmail.com>
2024-01-06 23:21:02 +0800

    changes to input

--- components/input.js
+++ components/input.js
@@ -10,6 +10,7 @@ import {
 
   function Input() {
     const [input, setInput] = useState("");
+    const [loading, setLoading] = useState(false);
     const [selectedFile, setSelectedFile] = useState(null);
     const filePickerRef = useRef(null);
     const [showEmojis, setShowEmojis] = useState(false);
@@ -42,10 +43,10 @@ import {
         }`}
       >
         <img
-          src={session.user.image}
+          src="https://rb.gy/ogau5a"
           alt=""
           className="h-11 w-11 rounded-full cursor-pointer"
-          onClick={signOut}
+        //   onClick={signOut}
         />
         <div className="divide-y divide-gray-700 w-full">
           <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
@@ -118,7 +119,7 @@ import {
               <button
                 className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
                 disabled={!input && !selectedFile}
-                onClick={sendPost}
+                // onClick={sendPost}
               >
                 Tweet
               </button>
