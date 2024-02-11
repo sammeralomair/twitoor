18122243450efbf9e9482b4d27e2f7ce8c558ac1
sammeralomair <omair.sao@gmail.com>
2024-02-11 20:48:24 +0300

    wrapping app in recoil state

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import "../styles.css/global.css";
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps }) {
-    return (
-        <SessionProvider>
-            <Component {...pageProps} />
-        </SessionProvider>
-    );
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    // `session` comes from `getServerSideProps` or `getInitialProps`.
+    // Avoids flickering/session loading on first load.
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
-
-export default MyApp
